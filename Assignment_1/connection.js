237e3dc3ea092251d35f8b06ed5dae476ee65c5b
Aparna Bhat <bhat_aparna1@yahoo.co.in>
2020-08-26 15:16:37 -0500

    Update connection.js

--- Assignment_1/connection.js
+++ Assignment_1/connection.js
@@ -1,5 +1,5 @@
 const ibmdb = require("ibm_db");
-const connStr= "DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-dal09-12.services.dal.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=foobar;PWD=foobar;" //Update this with your credentials
+const connStr= "DATABASE=BLUDB;HOSTNAME=Your_Hostname;PORT=50000;PROTOCOL=TCPIP;UID=foobar;PWD=foobar;" //Update this with your credentials
 const conn = ibmdb.openSync(connStr);
 
 module.exports = conn;
